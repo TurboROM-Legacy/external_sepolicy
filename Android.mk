0329c445db36799cd26e88357335c8153704ae50
Alex Naidis <alex.naidis@linux.com>
2016-01-13 21:00:43 +0100

    Force shipping_builds to false
    * as I have no clue what $CYNGN_TARGET is
    * We will force this to false to make the sepolicy more fiendly..
    
    Change-Id: I49a2fde87ca349e6bdd504312fcbdc79fc010e0a
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- Android.mk
+++ Android.mk
@@ -77,7 +77,7 @@ $(sepolicy_policy.conf) : $(call build_policy, $(sepolicy_build_files))
 	@mkdir -p $(dir $@)
 	$(hide) m4 -D mls_num_sens=$(PRIVATE_MLS_SENS) -D mls_num_cats=$(PRIVATE_MLS_CATS) \
 		-D target_build_variant=$(TARGET_BUILD_VARIANT) \
-		-D shipping_build=$(CYNGN_TARGET) \
+		-D shipping_build=false \
 		-s $^ > $@
 	$(hide) sed '/dontaudit/d' $@ > $@.dontaudit
 
